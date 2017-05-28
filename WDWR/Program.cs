ce634938fe45bb49dec87281c89308e30de62f25
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2017-05-28 19:32:22 +0200

    Vector Generation working + English

--- WDWR/Program.cs
+++ WDWR/Program.cs
@@ -12,73 +12,75 @@ namespace WDWR
     {
         static void Main(string[] args)
         {
-            double[] cena = { 170, 170, 170 };
-            double[] cenaMag = { 10, 10, 10 };
-            double[] twardosc = { 8.4, 6.2, 2.0 };
-            double[] twardosc3 = { 3,3,3 };
-            double[] twardosc6 = { 6,6,6 };
-            double[][] kosztOleju = new double[2][];
-            kosztOleju[0] = new double[3] { 116, 102, 113 }; 
-            kosztOleju[1] = new double[3] { 100, 107, 110 }; // TEMP
+            double[] price = { 170, 170, 170 };
+            double[] priceStorage = { 10, 10, 10 };
+            double[] hardness = { 8.4, 6.2, 2.0 };
+            double[] hardness3 = { 3,3,3 };
+            double[] hardness6 = { 6,6,6 };
+            double[][] oilCost = new double[2][];
+            oilCost[0] = new double[3] { 116, 102, 113 }; 
+            oilCost[1] = new double[3] { 100, 107, 110 }; // TEMP
 
-            INumVar[][] olejMag = new INumVar[3][];
-            INumVar[][] olejKup = new INumVar[2][];
-            INumVar[][] olejRaf = new INumVar[2][]; // [msc][A-C]
+            oilCost = GenerateRandomVector();
+
+            INumVar[][] oilStore = new INumVar[3][];
+            INumVar[][] oilBuy = new INumVar[2][];
+            INumVar[][] oilProduce = new INumVar[2][]; // [msc][A-C]
             Cplex cplex = new Cplex();
             for(int i=0;i<3;i++)
             {
-                olejMag[i] = new INumVar[3];
+                oilStore[i] = new INumVar[3];
                 for (int j = 0; j < 3; j++)
                 {
-                    olejMag[i][j] = cplex.NumVar(0, 800); // Ograniczenia na pojemność magazynu                    
+                    oilStore[i][j] = cplex.NumVar(0, 800); // Ograniczenia na pojemność magazynu                    
                 }                
             }
             for (int i = 0; i < 2; i++)
             {
-                olejKup[i] = new INumVar[3];
-                olejRaf[i] = new INumVar[3];
+                oilBuy[i] = new INumVar[3];
+                oilProduce[i] = new INumVar[3];
                 for (int j = 0; j < 3; j++) // Ograniczenia na możliwości rafinacji
                 {
                     if (j != 2)
                     {
-                        olejRaf[i][j] = cplex.NumVar(0, 220);
+                        oilProduce[i][j] = cplex.NumVar(0, 220);
                     }
                     else
                     {
-                        olejRaf[i][j] = cplex.NumVar(0, 270);                       
+                        oilProduce[i][j] = cplex.NumVar(0, 270);                       
                     }
-                    olejKup[i][j] = cplex.NumVar(0, 1000);
+                    oilBuy[i][j] = cplex.NumVar(0, 1000);
                 }
             }
             ILinearNumExpr mean = cplex.LinearNumExpr();
             for (int i = 0; i < 2; i++)
             {
-                cplex.Ge(cplex.ScalProd(twardosc3, olejRaf[i]), cplex.ScalProd(twardosc, olejRaf[i])); // Ograniczenie na twardosc 
-                cplex.Le(cplex.ScalProd(twardosc, olejRaf[i]),cplex.ScalProd(twardosc6, olejRaf[i]));
+                cplex.Ge(cplex.ScalProd(hardness3, oilProduce[i]), cplex.ScalProd(hardness, oilProduce[i])); // Hardness 
+                cplex.Le(cplex.ScalProd(hardness, oilProduce[i]),cplex.ScalProd(hardness6, oilProduce[i]));
             }
             for (int i = 0; i < 3; i++)
             {
-                cplex.AddEq(olejMag[0][i], 200.0); // Ograniczenie na stan magazynu w grudniu
-                cplex.AddEq(olejMag[2][i], 200.0); // Ograniczenie na stan magazynu w lutym
-                cplex.AddEq(olejMag[1][i], cplex.Sum(cplex.Diff(olejKup[0][i], olejRaf[0][i]),olejMag[0][i]));
-                cplex.AddEq(olejMag[2][i], cplex.Sum(cplex.Diff(olejKup[1][i], olejRaf[1][i]), olejMag[1][i]));
+                cplex.AddEq(oilStore[0][i], 200.0); // Ograniczenie na stan magazynu w grudniu
+                cplex.AddEq(oilStore[2][i], 200.0); // Ograniczenie na stan magazynu w lutym
+                cplex.AddEq(oilStore[1][i], cplex.Sum(cplex.Diff(oilBuy[0][i], oilProduce[0][i]),oilStore[0][i]));
+                cplex.AddEq(oilStore[2][i], cplex.Sum(cplex.Diff(oilBuy[1][i], oilProduce[1][i]), oilStore[1][i]));
             }
 
-            cplex.AddMaximize(cplex.Diff(cplex.Diff(cplex.Sum(cplex.ScalProd(cena,olejRaf[0]), cplex.ScalProd(cena,olejRaf[1])),cplex.Sum(cplex.ScalProd(cenaMag,olejMag[0]), cplex.ScalProd(cenaMag,olejMag[1]), cplex.ScalProd(cenaMag, olejMag[2]))),
-                cplex.Sum(cplex.ScalProd(kosztOleju[0], olejRaf[0]), cplex.ScalProd(kosztOleju[1], olejRaf[1]))));
-
-            Console.WriteLine("licze");
+            cplex.AddMaximize(cplex.Diff(cplex.Diff(cplex.Sum(cplex.ScalProd(price,oilProduce[0]), cplex.ScalProd(price,oilProduce[1])),cplex.Sum(cplex.ScalProd(priceStorage,oilStore[0]), cplex.ScalProd(priceStorage,oilStore[1]), cplex.ScalProd(priceStorage, oilStore[2]))),
+                cplex.Sum(cplex.ScalProd(oilCost[0], oilProduce[0]), cplex.ScalProd(oilCost[1], oilProduce[1]))));
+            
             if (cplex.Solve())
             {
                 System.Console.WriteLine();
                 System.Console.WriteLine("Solution status = " + cplex.GetStatus());
                 System.Console.WriteLine();
-                System.Console.WriteLine(" cost = " + cplex.ObjValue);
-                for (int j = 0; j < 2; j++)
+                System.Console.WriteLine("  = " + cplex.ObjValue);
+                Console.WriteLine();
+                for (int i = 0; i < 2; i++)
                 {
-                    for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
                     {
-                        System.Console.WriteLine(" olejRaf[" + j + "][" + i + "] = " + cplex.GetValue(olejRaf[j][i]));
+                        Console.WriteLine(" oilCost[" + i + "][" + j + "] = " + oilCost[i][j]);
                     }
                 }
                 Console.WriteLine();
@@ -86,7 +88,15 @@ namespace WDWR
                 {
                     for (int i = 0; i < 3; i++)
                     {
-                        System.Console.WriteLine(" olejKup[" + j + "][" + i + "] = " + cplex.GetValue(olejKup[j][i]));
+                        System.Console.WriteLine(" oilProduce[" + j + "][" + i + "] = " + cplex.GetValue(oilProduce[j][i]));
+                    }
+                }
+                Console.WriteLine();
+                for (int j = 0; j < 2; j++)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        System.Console.WriteLine(" oilBuy[" + j + "][" + i + "] = " + cplex.GetValue(oilBuy[j][i]));
                     }
                 }
                 Console.WriteLine();
@@ -94,7 +104,7 @@ namespace WDWR
                 {
                     for (int i = 0; i < 3; i++)
                     {
-                        System.Console.WriteLine(" olejMag[" + j + "][" + i + "] = " + cplex.GetValue(olejMag[j][i]));
+                        System.Console.WriteLine(" oilStore[" + j + "][" + i + "] = " + cplex.GetValue(oilStore[j][i]));
                     }
                 }
 
@@ -102,24 +112,24 @@ namespace WDWR
             }
             Console.ReadKey();
         }
-        static double[][] GenerujWektorLosowy()
+        static double[][] GenerateRandomVector()
         {
-            double[] wektorWartosciOczekiwanych = { 116, 102, 113, 100, 107, 110 };
-            double[] diagonalnaMacierzyKowariancji = { 1, 36, 4, 49, 16, 9 };
-            double[][] kosztOleju = new double[2][];
+            double[] excpectationVector = { 116, 102, 113, 100, 107, 110 };
+            double[] variance = { 1, 36, 4, 49, 16, 9 };
+            double[][] oilCost = new double[2][];
             for (int i = 0; i < 2; i++)
             {
-                kosztOleju[i] = new double[3];
+                oilCost[i] = new double[3];
                 for (int j = 0; j < 3; j++)
                 {
                     do
                     {
-                        kosztOleju[i][j] = StudentT.Sample(wektorWartosciOczekiwanych[i + j], Math.Sqrt(diagonalnaMacierzyKowariancji[i + j]), 4);
+                        oilCost[i][j] = StudentT.Sample(excpectationVector[i + j], Math.Sqrt(variance[i + j]), 4);
                     }
-                    while (80 <= kosztOleju[i][j] && kosztOleju[i][j] <= 120);
+                    while (80 >= oilCost[i][j] && oilCost[i][j] >= 120);                    
                 }
             }
-            return null;
+            return oilCost;
         }
     }
 }
